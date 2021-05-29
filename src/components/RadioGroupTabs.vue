1210af795539ee19847a4798d4efa0eae3e31660
Egor Shevchuk <efolkaras@gmail.com>
2021-05-29 23:27:20 +0300

    feat: add radio group component

---
+++ src/components/RadioGroupTabs.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="input-wrapper input-wrapper--radio-group">
+    <h4>{{ title }}</h4>
+    <template v-for="tab in tabs">
+      <input
+        :id="tab.id"
+        type="radio"
+        :name="name"
+        :value="tab.value"
+        :key="`input-${tab.key}`"
+        :checked="tab.value === value"
+        @input="$emit('input', tab.value)"
+      />
+      <label :key="`label-${tab.key}`" :for="tab.id">{{ tab.title }}</label>
+    </template>
+  </div>
+</template>
+
+<script>
+import "../assets/css/blocks/input-wrapper.css";
+
+export default {
+  name: "RadioGroupTabs",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    tabs: {
+      type: Array,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped></style>
