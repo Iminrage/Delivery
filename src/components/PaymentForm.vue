4203bbf51ee60af6a2f7a260f91c6981525d8d57
Egor Shevchuk <efolkaras@gmail.com>
2021-05-30 15:41:42 +0300

    feat: separate PaymentForm

---
+++ src/components/PaymentForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h3>Способ оплаты</h3>
+    <div
+      class="input-wrapper input-wrapper--radio-group input-wrapper--payment-method"
+    >
+      <input
+        id="pick-up-card"
+        type="radio"
+        name="payment-method"
+        value="card"
+        checked
+      />
+      <label for="pick-up-card">Карта</label>
+      <input
+        id="pick-up-cash"
+        type="radio"
+        name="payment-method"
+        value="cash"
+      />
+      <label for="pick-up-cash">Наличные курьеру</label>
+    </div>
+    <PaymentCardNumber @input="handleCardNumberChange" />
+    <div class="input-wrapper input-wrapper--input">
+      <h4>Номер телефона</h4>
+      <input id="pick-up-user-phone" type="text" placeholder="+7 (" required />
+      <label for="pick-up-user-phone" aria-label="Укажите телефон"></label>
+      <div class="input-wrapper__error">Укажите телефон</div>
+    </div>
+    <span class="form__info"
+      >Товар на складе будет привязан к номеру телефона. В пункте выдачи
+      назовите номер телефона, чтобы получить ваш заказ.</span
+    >
+  </div>
+</template>
+
+<script>
+import PaymentCardNumber from "./PaymentCardNumber";
+
+export default {
+  name: "PaymentForm",
+  components: {
+    PaymentCardNumber,
+  },
+  methods: {
+    handleCardNumberChange(data) {
+      console.log(data);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/ThePickup.vue
+++ src/components/ThePickup.vue
@@ -16,42 +16,7 @@
         v-model="selectedPickPoint"
       />
       <TheMap :lat="pickPointLatitude" :long="pickPointLongitude" />
-      <h3>Способ оплаты</h3>
-      <div
-        class="input-wrapper input-wrapper--radio-group input-wrapper--payment-method"
-      >
-        <input
-          id="pick-up-card"
-          type="radio"
-          name="payment-method"
-          value="card"
-          checked
-        />
-        <label for="pick-up-card">Карта</label>
-        <input
-          id="pick-up-cash"
-          type="radio"
-          name="payment-method"
-          value="cash"
-        />
-        <label for="pick-up-cash">Наличные курьеру</label>
-      </div>
-      <PaymentCardNumber></PaymentCardNumber>
-      <div class="input-wrapper input-wrapper--input">
-        <h4>Номер телефона</h4>
-        <input
-          id="pick-up-user-phone"
-          type="text"
-          placeholder="+7 ("
-          required
-        />
-        <label for="pick-up-user-phone" aria-label="Укажите телефон"></label>
-        <div class="input-wrapper__error">Укажите телефон</div>
-      </div>
-      <span class="form__info"
-        >Товар на складе будет привязан к номеру телефона. В пункте выдачи
-        назовите номер телефона, чтобы получить ваш заказ.</span
-      >
+      <PaymentForm @input="handlePaymentFormChange" />
       <div class="form__submit-block">
         <button class="form__submit-btn" type="submit" disabled>
           Заказать
@@ -70,14 +35,14 @@
 <script>
 import RadioGroupTabs from "./RadioGroupTabs";
 import TheMap from "./TheMap";
-import PaymentCardNumber from "./PaymentCardNumber";
+import PaymentForm from "./PaymentForm";
 
 export default {
   name: "ThePickup",
   components: {
     RadioGroupTabs,
     TheMap,
-    PaymentCardNumber
+    PaymentForm,
   },
   props: {
     supportedCities: {
@@ -109,6 +74,11 @@ export default {
     },
     pickPointLongitude: function () {
       return this.selectedPickPoint[1];
+    },
+  },
+  methods: {
+    handlePaymentFormChange(paymentData) {
+      console.log(paymentData);
     },
   },
   watch: {
